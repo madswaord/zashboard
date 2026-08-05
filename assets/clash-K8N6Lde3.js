a58ddbe6854f6e138f972a994ef10ac1e75f6f4f
madswaord <madswaord@users.noreply.github.com>
2026-08-05 20:29:17 +0000

    deploy: 3fc59531549499540be777956f1daa9863ba12c8

---
+++ assets/clash-K8N6Lde3.js
@@ -0,0 +1,1 @@
+import{aR as s,aT as t,aU as o}from"./index-P6e0DSMx.js";const n=async()=>{s.value=(await t()).data},c=async a=>{await o(a),n()};export{n as fetchConfigs,c as updateConfigs};
---
+++ assets/singbox-d4SQZQq3.js
@@ -0,0 +1,1 @@
+import{aV as e,aW as a}from"./index-P6e0DSMx.js";const s=async()=>{e.value=[],a.value=[]};export{s as fetchRules};
