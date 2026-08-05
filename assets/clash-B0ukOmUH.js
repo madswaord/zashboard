e2039e2a219b0caf4577291125bdef0b2425201e
madswaord <madswaord@users.noreply.github.com>
2026-08-05 20:29:14 +0000

    deploy: 3fc59531549499540be777956f1daa9863ba12c8

---
+++ assets/clash-B0ukOmUH.js
@@ -0,0 +1,1 @@
+import{aR as s,aT as t,aU as o}from"./index-CAwsom50.js";const n=async()=>{s.value=(await t()).data},c=async a=>{await o(a),n()};export{n as fetchConfigs,c as updateConfigs};
---
+++ assets/singbox-BCv7pwCi.js
@@ -0,0 +1,1 @@
+import{aV as e,aW as a}from"./index-CAwsom50.js";const s=async()=>{e.value=[],a.value=[]};export{s as fetchRules};
